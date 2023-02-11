7a4c56e3c65c9631a77be2284d958c54498ea0ce
chandu <kanchisekhar5324@gmail.com>
2023-02-11 13:03:14 +0530

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css";
 
-function App() {
+function BooksManagementForm() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [isbn, setIsbn] = useState('');
+  const [records, setRecords] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newRecord = { title, author, isbn };
+    setRecords([...records, newRecord]);
+    setTitle('');
+    setAuthor('');
+    setIsbn('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="APP">
+    <div class = "title">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <br></br>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </div>
+        <div className= "author">
+          <label htmlFor="Author">Author:</label>
+          <br></br>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(event) => setAuthor(event.target.value)}
+          />
+        </div>
+        <div className="isbn">
+          <label htmlFor="isbn">ISBN:</label>
+          <br></br>
+          <input
+            type="text"
+            id="isbn"
+            value={isbn}
+            onChange={(event) => setIsbn(event.target.value)}
+            
+          />
+        </div>
+        <br></br>
+        <button type="submit">Add Record</button>
+      </form>
+      <div className= "records">
+        <h2>Records:</h2>
+        {records.length > 0 ? (
+          <div className="display">
+          <table>
+            <thead>
+              <tr>
+              
+                <th>Title</th>
+                <th>Author</th>
+                <th>ISBN</th>
+                
+              </tr>
+            </thead>
+            <tbody>
+              {records.map((record, index) => (
+                <tr key={index}>
+                  <td>{record.title}</td>
+                  <td>{record.author}</td>
+                  <td>{record.isbn}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          </div>
+        ) : (
+          <p>No records yet.</p>
+          
+        )}
+        {records.length > 0 && (
+          <div>
+            <button onClick={() => setRecords([])}>Remove All</button>
+          </div>
+        )}
+      </div>
+    </div>
     </div>
   );
 }
 
-export default App;
+export default BooksManagementForm;
